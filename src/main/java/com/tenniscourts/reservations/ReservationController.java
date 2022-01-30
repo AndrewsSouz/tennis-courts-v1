b01238e0cd71bac73cfdb9d6ac2d2ba2c1364752
Andrews Souza Ferreira <ansouza@grupodimed.com.br>
2022-01-30 13:46:22 -0300

    Add reservation paths

--- src/main/java/com/tenniscourts/reservations/ReservationController.java
+++ src/main/java/com/tenniscourts/reservations/ReservationController.java
@@ -5,25 +5,32 @@ import com.tenniscourts.reservations.model.CreateReservationRequestDTO;
 import com.tenniscourts.reservations.model.ReservationDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
+@RequestMapping("/reservation")
 public class ReservationController extends BaseRestController {
 
     private final ReservationService reservationService;
 
+    @PostMapping
     public ResponseEntity<Void> bookReservation(CreateReservationRequestDTO createReservationRequestDTO) {
         return ResponseEntity.created(locationByEntity(reservationService.bookReservation(createReservationRequestDTO).getId())).build();
     }
 
-    public ResponseEntity<ReservationDTO> findReservation(Long reservationId) {
+    @GetMapping("/{reservationId}")
+    public ResponseEntity<ReservationDTO> findReservation(@PathVariable Long reservationId) {
         return ResponseEntity.ok(reservationService.findReservation(reservationId));
     }
 
-    public ResponseEntity<ReservationDTO> cancelReservation(Long reservationId) {
+    @PatchMapping("/cancel/{reservationId}")
+    public ResponseEntity<ReservationDTO> cancelReservation(@PathVariable Long reservationId) {
         return ResponseEntity.ok(reservationService.cancelReservation(reservationId));
     }
 
-    public ResponseEntity<ReservationDTO> rescheduleReservation(Long reservationId, Long scheduleId) {
+    @PatchMapping("/reschedule")
+    public ResponseEntity<ReservationDTO> rescheduleReservation(@RequestParam Long reservationId,
+                                                                @RequestParam Long scheduleId) {
         return ResponseEntity.ok(reservationService.rescheduleReservation(reservationId, scheduleId));
     }
 }
